a868caafa4a38c40b7c1d0a0257c057c0aec0671
Federico Marcos <marcosfede@gmail.com>
2016-12-01 00:41:03 -0300

    cleaned result display functions

--- src/components/Suggestions/LocationResult.jsx
+++ src/components/Suggestions/LocationResult.jsx
@@ -3,13 +3,20 @@ import {ListItem} from 'material-ui/List'
 import AccessIcon from 'material-ui/svg-icons/device/access-time'
 import {grey500} from 'material-ui/styles/colors'
 
-const cell = (result, type) =>
+const filterByType = (result, type) =>
     result.address_components.find((component) => component.types.includes(type))
+
+const getPrimaryText = result => {
+  return `${filterByType(result, 'route').long_name} n ${filterByType(result, 'street_number').long_name}`
+}
+const getSecondaryText = result => {
+  return `${filterByType(result, 'locality').long_name}, ${filterByType(result, 'administrative_area_level_1').long_name}`
+}
 
 const LocationResult = ({result}) => (
   <ListItem
-    primaryText={cell(result, 'route').long_name + ' ' + cell(result, 'street_number').long_name}
-    secondaryText={cell(result, 'locality').long_name + ', ' + cell(result, 'administrative_area_level_1').long_name}
+    primaryText={getPrimaryText(result)}
+    secondaryText={getSecondaryText(result)}
     leftIcon={<AccessIcon color={grey500} />}
         />
 )
