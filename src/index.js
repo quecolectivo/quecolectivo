7cdf62b7c61854b205b0e27767d4348ddf97d187
Federico Marcos <marcosfede@gmail.com>
2016-12-03 17:09:52 -0300

    refactored to use react-create-app config

--- src/index.js
+++ src/index.js
@@ -18,6 +18,33 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
+
 if (process.env.NODE_ENV === 'production') {
-  require('offline-plugin/runtime').install() // eslint-disable-line global-require
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./service-worker.js', {
+      scope: './'
+    })
+      .then(function (registration) {
+        registration.onupdatefound = function () {
+          if (navigator.serviceWorker.controller) {
+            var installingWorker = registration.installing
+            installingWorker.onstatechange = function () {
+              switch (installingWorker.state) {
+                case 'installed':
+                  break
+                case 'redundant':
+                  throw new Error('The installing ' +
+                                  'service worker became redundant.')
+                default:
+                  // Ignore
+              }
+            }
+          }
+        }
+      }).catch(function (e) {
+        console.error('Error during service worker registration:', e)
+      })
+  } else {
+    console.log('service worker is not supported')
+  }
 }
