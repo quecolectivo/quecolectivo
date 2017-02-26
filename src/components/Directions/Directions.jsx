c31e3f693328972b755f8963288d26c59f4ba2a6
Federico Marcos <marcosfede@gmail.com>
2017-02-26 17:01:20 -0300

    changed strategy on component styling

--- src/components/Directions/Directions.jsx
+++ src/components/Directions/Directions.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import LocationInput from './LocationInput'
 import './Directions.css'
+import Paper from 'material-ui/Paper';
 
 const Directions = ({ children }) => (
   <directions>
     <LocationInput />
-    <content>
+    <content className="pulldown">
+      <Paper zDepth={1}>
       { children }
+      </Paper>
     </content>
   </directions>
 )
